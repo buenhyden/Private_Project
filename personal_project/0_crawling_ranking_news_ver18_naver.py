078b83bc6a8bc3ef2183ac88de3a4efe413e22e2
buenhygoon <chochyjj@gmail.com>
2018-02-03 16:43:11 +0900

    no message

--- personal_project/0_crawling_ranking_news_ver18_naver.py
+++ personal_project/0_crawling_ranking_news_ver18_naver.py
@@ -253,9 +253,13 @@ class GetContents:
                    range(1, numPage + 1)]
         comment = list(itertools.chain.from_iterable(comment))
         comment = pd.DataFrame(comment)
-        comment = comment[['contents', 'antipathyCount', 'sympathyCount']]
-        comment.rename({'antipathyCount': '비공감',
-                        'sympathyCount': '공감'}, inplace=True, axis=1)
+        if comment.shape[0] != 0:
+            comment = comment[['contents', 'antipathyCount', 'sympathyCount']]
+            comment.rename({'antipathyCount': '비공감',
+                            'contents': 'comments',
+                            'sympathyCount': '공감'}, inplace=True, axis=1)
+        else:
+            comment = pd.DataFrame(columns=['comments', '공감', '비공감'])
         return comment, totalCount, comment.shape[0]
 
 def Main_Naver(date):
